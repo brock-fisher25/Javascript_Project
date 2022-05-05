3e0782298c5690538e2bf9c0b72bd71eb90155fd
brock-fisher25 <94400152+brock-fisher25@users.noreply.github.com>
2022-05-05 12:46:27 -0600

    Update MainGame.js

--- code/MainGame.js
+++ code/MainGame.js
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Canvas</title>
-    <script src="MainGame.js" defer></script>
-    <link href="MainGame.css" rel="stylesheet">
-  </head>
-  <body>
-      <canvas class="myCanvas">
-        <p>Main Game</p>
-      </canvas>
-  </body>
-</html>
+const canvas = document.querySelector('.myCanvas');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
