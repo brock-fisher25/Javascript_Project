a15c8d6f63de1c30302f8f7c002a1db581e85a92
brock-fisher25 <94400152+brock-fisher25@users.noreply.github.com>
2022-05-05 13:27:01 -0600

    Update MainGame.js

--- code/MainGame.js
+++ code/MainGame.js
@@ -1,3 +1,6 @@
 const canvas = document.querySelector('.myCanvas');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+const context = canvas.getContext('2d');
+context.fillStyle = 'rgb(0, 0, 0)';
+context.fillRect(0, 0, width, height);
