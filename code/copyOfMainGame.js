446704dde373605e5da318013c41f8c8ff58f08d
brock-fisher25 <94400152+brock-fisher25@users.noreply.github.com>
2022-05-09 14:53:42 -0600

    Delete copyOfMainGame.js

--- code/copyOfMainGame.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.querySelector('.myCanvas');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-const context = canvas.getContext('2d');
-context.fillStyle = 'rgb(0, 0, 0)';
-context.fillRect(0, 0, width, height);
-context.fillStyle = 'rgb(255, 0, 0)';
-context.fillRect(50, 50, 100, 150);
