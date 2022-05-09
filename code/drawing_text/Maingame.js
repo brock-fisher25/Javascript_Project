99e7bf69cd33e6be49ee48778a1b8fa9571c2600
brock-fisher25 <94400152+brock-fisher25@users.noreply.github.com>
2022-05-09 14:34:06 -0600

    Update Maingame.js

--- code/drawing_text/Maingame.js
+++ code/drawing_text/Maingame.js
@@ -5,3 +5,11 @@ const context = canvas.getContext('2d');
 // first rectangle
 context.fillStyle = 'rgb(0, 0, 0)';
 context.fillRect(0, 0, width, height);
+// text
+context.strokeStyle = 'white';
+context.lineWidth = 1;
+context.font = '36px arial';
+context.strokeText('Canvas text', 50, 50);
+context.fillStyle = 'red';
+context.font = '48px georgia';
+context.fillText('Canvas text', 50, 150);
